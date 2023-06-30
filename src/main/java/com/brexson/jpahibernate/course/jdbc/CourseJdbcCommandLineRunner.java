682399cb27c0c542fcbb3b6905b648af687a9a47
steevecode <stevecode@yahoo.com>
2023-06-30 09:32:25 +0100

    Iteration 3: Querying Data Using Spring JDBC

--- src/main/java/com/brexson/jpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/brexson/jpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -17,5 +17,8 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
         repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
         repository.insert(new Course(3, "Learn DevOps Now!", "in28minutes"));
         repository.deleteById(3);
+        System.out.println(repository.findById(1));
+        System.out.println(repository.findById(2));
+
     }
 }
--- src/main/java/com/brexson/jpahibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/brexson/jpahibernate/course/jdbc/CourseJdbcRepository.java
@@ -2,6 +2,7 @@ package com.brexson.jpahibernate.course.jdbc;
 
 import com.brexson.jpahibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,6 @@ public class CourseJdbcRepository {
                 insert into course(id, name, author) 
                 values(?,?,?);
             """;
-
     public void insert(Course course){
       jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(),course.getAuthor());
     }
@@ -22,8 +22,15 @@ public class CourseJdbcRepository {
             """
                 delete from course where id = ?;
             """;
-
     public void deleteById(long id){
         jdbcTemplate.update(DELETE_QUERY, id);
+    }
+    private static String SELECT_QUERY =
+            """
+                select * from course where id = ?;
+            """;
+    public Course findById(long id){
+        // ResultSet => Bean => RowMapper
+        return jdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
     }
 }
